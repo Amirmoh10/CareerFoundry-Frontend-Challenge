7584d33dd00e71670928204e585731b9fb5ccf43
Amir Ghezala <mohamedghezala@gmail.com>
2022-12-13 21:05:28 +0400

    Use https for ipstack

--- src/constants.js
+++ src/constants.js
@@ -8,6 +8,6 @@ export const REQUEST = {
 export const COURSES_URL =
   "https://private-e05942-courses22.apiary-mock.com/courses";
 
-export const USER_LOCATION_URL = "http://api.ipstack.com/check";
+export const USER_LOCATION_URL = "https://api.ipstack.com/check";
 
 export const IPSTACK_KEY = "d76f04ddac0c69d98114b526849d6f87";
